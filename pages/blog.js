65562131475e7be68da87f74690eef000a012449
Kittisak Pharaphan <kittisak.py@gmail.com>
2018-12-10 02:19:10 +0700

    individual post

--- pages/blog.js
+++ pages/blog.js
@@ -1,12 +1,18 @@
 import Layout from "../components/Layout"
 import Link from "next/link"
 
+const PostLink = ({title}) => (
+    <li>
+        <Link href={`/post?title=${title}`}><a>{title}</a></Link>
+    </li>
+)
+
 export default () => (
     <Layout title="My blog">
         <ul>
-            <li>
-                <Link href="/post?title=React"><a>React Post</a></Link>
-            </li>
+            <PostLink title="react"/>
+            <PostLink title="angular"/>
+            <PostLink title="vue"/>
         </ul>
     </Layout>
 )
