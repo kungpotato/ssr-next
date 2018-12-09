167a0a0526932d3bcc3353489dbaa495cf8c0f7a
Kittisak Pharaphan <kittisak.py@gmail.com>
2018-12-09 21:51:49 +0700

    css scope and global

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import Link from "next/link"
 
 export default ({children, title}) => (
-    <div>
+    <div className="root">
         <header>
             <Link href="/"><a>Home</a></Link>
             <Link href="/about"><a>About</a></Link>
@@ -11,5 +11,40 @@ export default ({children, title}) => (
             {children}
 
         <footer>&copy; {new Date().getFullYear()}</footer>
+        <style jsx>{`
+            .root{
+                display: flex;
+                justify-content: center;
+                align-item: center;
+                flex-direction: column;
+            }
+            header{
+                width: 100%;
+                display: flex;
+                justify-content: space-around;
+                padding: 1em;
+                font-size: 1.2rem;
+                background: indigo;
+            }
+            header a{
+                color: darkgrey;
+                text-decoration: none
+            }
+            header a:hover{
+                font-weight: bold;
+                color: lightgrey;
+            }
+            footer{
+                padding: 1em;
+
+            }
+        `}</style>
+        <style global jsx>{`
+            body{
+                margin: 0;
+                font-size: 110%;
+                background: #f0f0f0;
+            }
+        `}</style>
     </div>
 )
