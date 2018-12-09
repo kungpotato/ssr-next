ed963b62416c1c17cdc464004b3994ded8111e4b
Kittisak Pharaphan <kittisak.py@gmail.com>
2018-12-10 01:21:49 +0700

    ใช้ getInitialProps โดยไม่ต้อง setstate
    stateless นั่นเอง

--- pages/about.js
+++ pages/about.js
@@ -1,26 +1,34 @@
 import Link from "next/link"
 import Layout from "../components/Layout"
 import {Component} from "react"
+import fetch from "isomorphic-unfetch"
 
 export default class About extends Component {
-  state = {
-    user: null
+
+  static getInitialProps () {
+      fetch('https://api.github.com/users/reedbarger')
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+        })
+
+        return { user: 'user'}
   }
 
-  componentDidMount () {
-    fetch('https://api.github.com/users/reedbarger')
-      .then(res => res.json())
-      .then(data => {
-        this.setState({
-          user: data
-        })
-      })
-  }
+  // componentDidMount () {
+  //   fetch('https://api.github.com/users/reedbarger')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       this.setState({
+  //         user: data
+  //       })
+  //     })
+  // }
 
   render () {
     return (
       <Layout title="About">
-        {JSON.stringify(this.state.user)}
+        {/* {JSON.stringify(this.state.user)} */}
         <Link href="/">
           <a>Go home</a>
         </Link>
@@ -30,3 +38,4 @@ export default class About extends Component {
     )
   }
 }
+
