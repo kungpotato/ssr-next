165af18ed795aaada493f342e8a14abec7f4f40a
Kittisak Pharaphan <kittisak.py@gmail.com>
2018-12-09 21:09:05 +0700

    test hire me mail

---
+++ pages/hireme.js
@@ -0,0 +1,10 @@
+const HireMe = () => (
+    <div>
+        <h1>Hire me</h1>
+        <p>about kungpotato
+            you can add me at{" "}
+            <a href="mailto:kungpotato@gmail.com">kungpotato@gmail.com</a>
+        </p>
+    </div>
+  )
+  export default HireMe
