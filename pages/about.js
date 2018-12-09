3ed2a69e5da33140315bd92433e6cbe50f6dee61
Kittisak Pharaphan <kittisak.py@gmail.com>
2018-12-10 01:11:29 +0700

    fetch data test
    - state
    - componentDidMount

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,32 @@
 import Link from "next/link"
 import Layout from "../components/Layout"
+import {Component} from "react"
 
-const About = () => (
-    <Layout title="About">
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-      <p>about kungpotato</p>
-      <img src="/static/test1.jpg"/>
-    </Layout>
-  )
-  export default About
+export default class About extends Component {
+  state = {
+    user: null
+  }
+
+  componentDidMount () {
+    fetch('https://api.github.com/users/reedbarger')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          user: data
+        })
+      })
+  }
+
+  render () {
+    return (
+      <Layout title="About">
+        {JSON.stringify(this.state.user)}
+        <Link href="/">
+          <a>Go home</a>
+        </Link>
+        <p>about kungpotato</p>
+        <img src="/static/test1.jpg"/>
+      </Layout>
+    )
+  }
+}
