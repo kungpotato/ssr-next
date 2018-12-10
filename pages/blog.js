3ec5bcd2b3fbdac0e9e271def90075493af462c2
Kittisak Pharaphan <kittisak.py@gmail.com>
2018-12-10 10:25:09 +0700

    clean url no query string

--- pages/blog.js
+++ pages/blog.js
@@ -1,18 +1,18 @@
 import Layout from "../components/Layout"
 import Link from "next/link"
 
-const PostLink = ({title}) => (
+const PostLink = ({title, slug}) => (
     <li>
-        <Link href={`/post?title=${title}`}><a>{title}</a></Link>
+        <Link as={`/${slug}`} href={`/post?title=${title}`}><a>{title}</a></Link>
     </li>
 )
 
 export default () => (
     <Layout title="My blog">
         <ul>
-            <PostLink title="react"/>
-            <PostLink title="angular"/>
-            <PostLink title="vue"/>
+            <PostLink slug="react-post" title="React Post"/>
+            <PostLink slug="angular-post" title="Angular Post"/>
+            <PostLink slug="vue-post" title="Vue Post"/>
         </ul>
     </Layout>
 )
